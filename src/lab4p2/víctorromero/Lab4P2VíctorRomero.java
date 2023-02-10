445dddc48c13fcdb28d25afcda66806479104752
VIRN2005 <virn2005@gmail.com>
2023-02-10 13:25:29 -0600

    Commit inicial

---
+++ src/lab4p2/víctorromero/Lab4P2VíctorRomero.java
@@ -0,0 +1,12 @@
+
+package lab4p2.víctorromero;
+
+public class Lab4P2VíctorRomero {
+
+    public static void main(String[] args) {
+       System.out.println("Gerardo Cano - 12151024\n");
+       System.out.println("Victor Romero - 12211079\n");
+
+    }
+    
+}
